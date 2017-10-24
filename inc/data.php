62e070924c5864ff7fe180c65f3f199e07c54555
Krista <krista.t.jekel@gmail.com>
2017-10-24 12:20:38 -0600

    Project page shows details

--- inc/data.php
+++ inc/data.php
@@ -1,39 +1,38 @@
 <?php
 
 $project = [];
-$project[01] = [ 
+$project[101] = [ 
     "title" => "Tribute Page",
       "img" => "#",
-      "src" => "https://codepen.io/Simpooly/full/WEyaam/",
-      "goal" => "yo",
-      "process" => "yo",
-      "troubles" => "yo",
-      "outcome" => "yo",
+      "url" => "https://codepen.io/Simpooly/full/WEyaam/",
+      "goal" => "The purpose of this project was tp practice HTML and CSS basics, This is the first project where both languaged were used together.",
+      "troubles" => "I had spent many weeeks leaning the basics of HTML, CSS, PHP, JavaScript, JQuery and more. As my first real project it was intersting to find that knowing the different parts and making the different parts work were two completely different things. I struggled getting the images of the books to link properly. I was doing what the documantation was telling me to, but it still wasnt working. In the end i found out it was not my code, but the URL. I found that Barns & Noble had a more helpful url to link to and used that instead.",
+      "thoughts" => "Even thoguh this was simple i was happy that i was learning how to use trial and error and was practicing being able to problem solve more instead of getting stuck on the problem.",
       "catagory" => " basic"  
 ];
-$project[02] = [
+$project[102] = [
     "title" => "Photography",
       "img" => "#",
-      "src" => "https://codepen.io/Simpooly/full/RZqMNR/",
+      "url" => "https://codepen.io/Simpooly/full/RZqMNR/",
       "catagory" => " basic"
 ];
-$project[03] = [
+$project[103] = [
     "title" => "Old Photography",
       "img" => "#",
-      "src" => "#",
+      "url" => "#",
       "catagory" => " basic"
 ];
-$project[04] = [
-    "title" => "Randome Quote",
+$project[201] = [
+    "title" => "Random Quote",
       "img" => "#",
       "src" => "https://codepen.io/Simpooly/full/KXyoeV/",
       "catagory" => "javascript"
 ];
 
-$project[05] = [
+$project[202] = [
     "title" => "Weather App",
       "img" => "#",
-      "src" => "https://codepen.io/Simpooly/pen/qPMjwG",
+      "url" => "https://codepen.io/Simpooly/pen/qPMjwG",
       "catagory" => "javascript"
 ];
 
--- project.php
+++ project.php
@@ -1,49 +1,44 @@
 
 <?php
-include('inc/data.php');
-include('inc/functions.php');
-//project.php?id=01
+    include('inc/data.php');
+    include('inc/functions.php');
+    //project.php?id=01
 
-if(isset($_GET["id"])){
-    $id = $_GET["id"];
-    if(isset($project[$id])){
-    $item = $project[$id];
+    if(isset($_GET["id"])){
+        $id = $_GET["id"];
+        if(isset($project[$id])){
+        $item = $project[$id];
+        }
     }
-}
 
-if(!isset($item)){
-    header("location:portfolio.php");
-    exit;
-}
+    if(!isset($item)){
+        //header() redirects to another page or location, look for different page
+        header("location:portfolio.php");
+        exit;
+    }
 
+    $pageTitle = $item["title"];
+    $section = null;
 
-$pageTitle = $item["title"];
-$section = null;
-include('inc/header.php');
+    include('inc/header.php');
 ?>
 
 <div>
-    <divclass="wrapper">
+    <div class="wrapper">
 
-    <h1><?php echo $pageTitle; ?></h1>
+        <h1><?php echo $pageTitle; ?></h1>
 
-    <div class="project-picture">
-        <img src="<?php echo $item["src"] ?>" alt="<?php echo $item["title"] ?>">
-    </div>
+        <!--<div class="project-picture">
+            <img src="<?php echo $item["img"] ?>" alt="<?php echo $item["title"] ?>">
+        </div> -->
 
-    
-    //goal
-    <p class="goal"></p>
-    //process
-    <p class="process"></p>
+        <p class ="goal"><?php echo $item["goal"]; ?></p><br>
 
-    //troubles
-    <p class=" troubles"></p>
-    //outscome
-    <p class="outcome"></p>
+        <p class ="trouble"><?php echo $item["troubles"]; ?></p><br>
+
+        <p class ="thoughts"><?php echo $item["thoughts"]; ?></p><br>
 
     </div>
-
 </div>
 
 <?php include('inc/footer.php'); ?>
