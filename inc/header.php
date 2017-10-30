fb398983ff146f1ed0e2f1e6e9f443c5c08bf336
Krista <krista.t.jekel@gmail.com>
2017-10-30 10:57:54 -0600

    sticky footer, toggle nav

--- inc/header.php
+++ inc/header.php
@@ -12,7 +12,7 @@ $company_name = 'SimpOoly';
     <link rel="stylesheet" href="inc/style.css" type="text/css">
 </head>
 
-<body >
+<body>
     <head>
         <div class="navigation">
             
@@ -30,7 +30,12 @@ $company_name = 'SimpOoly';
 					<div class="bar"></div>
 					<div class="bar"></div>
 				</div>
-				
+			<ul class="nav-small">
+                <li class="<?php if ($section == "home"){echo "on";}?>"><a href="index.php">Home</a></li>
+                <li class="<?php if ($section == "portfolio"){echo "on";}?>"><a href="portfolio.php">Portfolio</a></li>
+                <li class="<?php if ($section == "about"){echo "on";}?>"><a href="about.php">About</a></li>
+                <li class="<?php if ($section == "contact"){echo "on";}?>"><a href="contact.php">Contact</a></li>            
+            </ul>
 		    </div> <!-- /nav-toggle -->
         </div>
         
--- index.php
+++ index.php
@@ -7,26 +7,27 @@ include('inc/header.php');
 
 
 ?>
-<div class="wrapper">
-    <div id="jumbotron">
-    <h2>It's <?php  echo $company_name; ?> delightful to meet you!</h2>
-    <h3>Desribe what you are/ do, web developer</h3>
-    </div> 
-       
+
+    <div class="wrapper">
+        <div id="jumbotron">
+        <h2>It's <?php  echo $company_name; ?> delightful to meet you!</h2>
+        <h3>Desribe what you are/ do, web developer</h3>
+        </div> 
+        
 
 
-    <div id="projects">
-        <ul id="gallery">
-            <?php 
-            $random = array_rand($project, 3);
-            
-                foreach($random as $id){
-                    echo get_item_html($id, $project[$id]);
-                }
-            ?>
-        </ul> 
-    </div> 
+        <div id="projects">
+            <ul id="gallery">
+                <?php 
+                $random = array_rand($project, 3);
+                
+                    foreach($random as $id){
+                        echo get_item_html($id, $project[$id]);
+                    }
+                ?>
+            </ul> 
+        </div> 
     </div><!-- wrapper end -->
-</body>
+
 
 <?php include('inc/footer.php'); ?>
