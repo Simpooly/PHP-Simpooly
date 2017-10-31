0941d5e04257bb588cada3b76661857a4f847ffb
Krista <krista.t.jekel@gmail.com>
2017-10-31 15:52:14 -0600

    trim, sanitize, and require input fields

--- contact.php
+++ contact.php
@@ -1,4 +1,35 @@
 <?php
+
+//loads the values from form, remove any spaces extra char, and filter
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
+    $email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
+    $message = trim(filter_input(INPUT_POST, "message", FILTER_SANITIZE_SPECIAL_CHARS));
+
+    //make sure fields are valid
+    if( $name == "" || $email == "" || $message == "" ){
+        echo "Please fill out required fields";
+        exit;
+    }
+
+    //see if hidden field is filled out--ROBOTS!!
+    if ( $_POST["address"] != ""){
+       echo "Bad form input";
+       exit;
+    }
+
+    echo "<pre>";
+    $email_body = "";
+    $email_body .= "Name: " . $name . "\n";
+    $email_body .= "Email: " . $email . "\n";
+    $email_body .= "Message: " . $message . "\n";
+    echo $email_body;
+    echo "</pre>";
+
+    //To Do send email!!
+    header("location:contact.php?status=thanks");
+}
+
 $pageTitle = "Contact";
 $section = "contact";
 
@@ -10,11 +41,17 @@ include('inc/header.php');
 <div class="wrapper">
 	
     <h1><?php echo $pageTitle; ?></h1>
+    <?php 
+        if(isset($_GET["status"]) && $_GET["status"] == "thanks") {
+            echo "<h2>Thank You, add js smiley or thumbs up? animation?</h2>";
+
+        } else{
+    ?>
     <p>If you have any questions or want to contact me, please feel free to send me an email.
     </p>
 
     <div class="contact-form">
-        <form action="process.php" method="post">
+        <form action="contact.php" method="post">
             <table>
                 <tr>
                     <th><label for="name">Name:</label></th>
@@ -23,6 +60,11 @@ include('inc/header.php');
                 <tr>
                     <th><label for="email">Email Address:</label></th>
                     <td><input id="email" type="text" name="email"></input></td>
+                </tr>
+                <tr style="display:none">
+                    <th><label for="address">Address:</label></th>
+                    <td><input id="address" type="text" name="address">
+                    <p>Please leave blank</p></input></td>
                 </tr>
                 <tr>
                     <th><label for="message">Message:</label></th>
@@ -33,6 +75,7 @@ include('inc/header.php');
 
             <input type="submit" value="Submit">
         </form>
+        <?php } ?>
     <div>
 </div><!-- wrapper end -->
 
