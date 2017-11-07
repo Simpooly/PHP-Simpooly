6e2e2729629d37ca5f852d5ba5c38255696cdd3a
Krista <krista.t.jekel@gmail.com>
2017-11-07 10:02:32 -0700

    text font css

--- about.php
+++ about.php
@@ -22,12 +22,14 @@ include('inc/header.php');
 			
 			<p>Simplicity with a quirk. As a self-starter in the computer science field, I have spent my free time doing online classes, tutorials, and tracks gaining knowledge of basic development languages. Improving and expanding on those skills I had the opportunity to provide web updates and data entry into various websites.
 			<br>
-			<br>
+			<br><p>
 			I am currently spending time working on personal projects to perfect and practice the detail orienting skills of HTML, CSS, JavaScript, jQuery. I was exposed to the world of PHP and Wordpress development and would like to dive in deeper into those topics as well. Through learning development on my own I have become conscious of soft skills such as time-management, habit forming, and self-motivation. 'Id like to develope...skill, i would consider myself well versed at..., i am familiar with..., presentations.. preperation is where i excell, not quick on your feet, time management is about prioritizing'
+</p>
 			<br>
 			<br>
+			<p>
 			Eager to learn more and gain skill sets in the development community I am planning on seeking out an internship or student position while taking on individual clients and opportunities in my free time.'something about direction you want to go, javascript?  profeshinal enthusiasm, enthusiastic, coordinated, develope, applications, bugs,algorithms, programming, coding, computer systems, customer needs, data aquisition, data entry,data migration, data processing, troubleshooting, deadlines, implementation, documentation,organizing, integration, systems,prototype, reliability, security, source code,statistical knowledge, configuration, servers,maitained, analyzed,contributed, created, designed, developed, formulated, identified,preformed, provided, resolved,'</p>
-			
+</p>
 
 			<br>
 			<br>
