b57dc20395260f0b6af73099834e61f8adcba583
Krista <krista.t.jekel@gmail.com>
2019-02-04 19:18:17 -0700

    JumbtronUpdated

--- index.php
+++ index.php
@@ -8,110 +8,413 @@ include('inc/header.php');
 
 ?>
 
-<!-- Top open page -->
+<style>
+/* -------------------------------------------------------------------------------- */
+/*  0. CSS Reset
+/* -------------------------------------------------------------------------------- */
 
-<section class="section-full vertical-cut full-height">
-    <div id="jumbotron">
-        <div class="home-logo">
-            <p class="logo">Simpooly</p>
-            <h2 class="logo2"> Web Development</h2>
-        </div>
+
+html, body { margin:0; padding:0;}
+
+h1, h2, h3, h4, h5, h6, p, blockquote, address, big, cite, code, em, font, img,
+, strike, sub, sup, li, ol, ul, fieldset, form, label, legend, button, table, caption, tr, th, td {
+    margin:0;
+    padding:0;
+    border:0;
+    font-weight:normal;
+    font-style:normal;
+    font-size:100%;
+    line-height:1;
+    font-family:inherit;
+    text-align:left;
+}
+
+table {
+    border-collapse:collapse;
+    border-spacing:0;
+}
+
+ol, ul { list-style:none; }
+
+blockquote:before, blockquote:after { content:""; }
+ /* .sitewide-wrapper.main-sitewide-wrapper {
+    display: none;
+  }*/
+/*  .home-page {
+    min-height: 1px;
+    border-bottom: 2px solid #ebebeb;
+    overflow: hidden;
+    -webkit-font-smoothing: antialiased;
+  }*/
+  /* -------------------------------------------------------------------------------- */
+  /*  1. General Style
+  /* -------------------------------------------------------------------------------- */
+ .active-card {
+    z-index: 1;
+    box-shadow: 0 5px 15px rgba(0,0,0,.19), 0 6px 6px rgba(0,0,0,.23), 0 1px 3px rgba(0,0,0,.24);
+  }
+  .passive-card{
+    margin: 0 7.5px 15px;
+    background-color: #fff;
+    box-shadow: 0 0 3px rgba(0,0,0,.12), 0 1px 3px rgba(0,0,0,.24);
+  }
+
+  .home-page a:hover,
+  .home-page a:focus {
+    text-decoration: none;
+  }
+
+
+  /*---------------------------------------------Clear Styles*/
+
+/*  .home-page .cs_container-crocs,
+  .home-page .cs_container-fluid,
+  .home-page .text-container,
+  .home-page .right-container {
+    padding-right: 0px;
+    padding-left:  0px;
+    margin-left: 0px;
+    margin-right: 0px;
+  }*/
+
+
+  /*----------------------------------------------Core Layout*/
+  .home-page .jumbotron h2{
+        /*font-size: 5.5em;*/
+        color:  #320d2d;
+        font-weight: 500;
+        background-color: #fff;
+        margin: 0px;
+        /*margin: 15px;*/
+        /*padding: 15px;*/
+        font-family: 'Milonga', cursive;
+        /*display:table-cell;*/
+        text-align:center;
+        /*width:100%;*/
+        /*vertical-align: middle;*/
+        /*border: 1px solid #303030;*/
+  }
+  .home-page .jumbotron h3{
+        margin: 0px;
+
+  }
+  .home-page .jumbotron{
+    background: rgb(50,13,45);
+    background: -moz-linear-gradient(90deg, rgba(50,13,45,1) 51%, rgba(255,255,255,1) 50%);
+    background: -webkit-linear-gradient(90deg, rgba(50,13,45,1) 51%, rgba(255,255,255,1) 50%);
+    background: linear-gradient(90deg, rgba(50,13,45,1) 51%, rgba(255,255,255,1) 50%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#320d2d",endColorstr="#ffffff",GradientType=1);
+    border-bottom: 1px solid #ebebeb;
+    /*height: 50%; /*500px*/*/
+    /*margin-top: 82px;*/
+  }
+  .home-page .jumbotron .logo{
+        /*font-size: 5.5em;*/
+      /*  color:  #320d2d;
+        font-weight: 500;*/
+        background-color: #fff;
+        /*margin: 7px;*/
+        padding: 7px;
+        /*font-family: 'Milonga', cursive;*/
+        /*display:table-cell;*/
+        text-align:center;
+        border: 1px solid #303030;
+        /*width:100%;*/
+        /*vertical-align: middle;*/
+        /*border: 1px solid grey;*/
+  }
+  .home-page .wrapper{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+    width: 980px;
+    margin: 0 auto;
+  }
+  .home-page .flex-row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+  }
+  .home-page .flex-column {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  .home-page .center-content{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /*height: 100%;*/
+  }
+
+  /*------------------------------------General Button Styles*/
+
+/*  .home-page a:hover .cx-button,
+  .home-page a:focus .cx-button {
+  .home-page .cx-button:hover,
+  .home-page .cx-button:focus {
+    color: #fff;
+    background-color: #804068;
+    border-color: #804068;
+  }
+  .home-page .cx-button {
+    color: #804068;
+    background-color: #fff;
+    border-color: #804068;
+    text-align: center;
+    font-size: 16px;
+    padding: 12px 0px 12px 0px;
+    border-width: 2px;
+    line-height: 1.0;
+  }
+  .home-page .btn {
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+  .home-page .btn-overall {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    align-items: center;
+  }*/
+
+  /*----------------------------------------------Text Styles*/
+
+  .home-page h2{
+    color: #320d2d;
+  }
+  .home-page h3{
+    color: #4F6D3C;
+  }
+  .home-page h4{
+    color: #999;
+  }
+  .home-page .text-title{
+    font-size: 18px;
+    line-height: 20px;
+    color: #999;
+    font-weight: 700;
+  }
+  .home-page .text-description{
+    font-size: 16px;
+    line-height: 18px;
+    color: #999;
+  }
+
+
+  /* Small devices (Phones, under 480px) */
+  @media (max-width: 480px) {
+    .home-page .jumbotron{
+/*      background: rgb(50,13,45);
+      background: -moz-linear-gradient(90deg, rgba(50,13,45,1) 50%, rgba(255,255,255,1) 50%);
+      background: -webkit-linear-gradient(90deg, rgba(50,13,45,1) 50%, rgba(255,255,255,1) 50%);
+      background: linear-gradient(90deg, rgba(50,13,45,1) 50%, rgba(255,255,255,1) 50%);
+      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#320d2d",endColorstr="#ffffff",GradientType=1);
+      border-bottom: 1px solid #ebebeb;*/
+      height: 100vh; /*500px*/
+      /*margin-top: 82px;*/
+    }
+    .home-page .jumbotron h2{
+          font-size: 16vw;
+        /*  color:  #320d2d;
+          font-weight: 500;*/
+          /*background-color: #fff;*/
+          /*margin: 3vw;*/
+          /*padding: 3vw;*/
+          /*font-family: 'Milonga', cursive;*/
+          /*display:table-cell;*/
+          /*text-align:center;*/
+          /*width:100%;*/
+          /*vertical-align: middle;*/
+          /*border: 1px solid grey;*/
+    }
+    .home-page .wrapper-logo{
+        padding: 5vw;
+        background-color: #fff;
+    }
+    .home-page .jumbotron .logo{
+          /*font-size: 5.5em;*/
+        /*  color:  #320d2d;
+          font-weight: 500;*/
+          background-color: #fff;
+          /*margin: 7px;*/
+          padding: 3vw;
+          /*font-family: 'Milonga', cursive;*/
+          /*display:table-cell;*/
+          text-align:center;
+          border: 1px solid #303030;
+          /*width:100%;*/
+          /*vertical-align: middle;*/
+          /*border: 1px solid grey;*/
+    }
+    .home-page .jumbotron h3{
+       font-size: 2.8vw;
+       text-transform: uppercase;
+       font-weight: bold;
+       text-align: center;
+
+       position: relative;
+       bottom: 2vw;
+       left: 12.5vw;
+          /*color:  #320d2d;*/
+    }
+  }
+
+  /* Extra Small Devices, Phones */
+  @media (min-width : 481px) {
+    .home-page .jumbotron .logo{
+          /*font-size: 5.5em;*/
+        /*  color:  #320d2d;
+          font-weight: 500;*/
+          background-color: #fff;
+          /*margin: 7px;*/
+          padding: 7px;
+          /*font-family: 'Milonga', cursive;*/
+          /*display:table-cell;*/
+          text-align:center;
+          border: 1px solid #303030;
+          /*width:100%;*/
+          /*vertical-align: middle;*/
+          /*border: 1px solid grey;*/
+    }
+    .home-page .jumbotron{
+/*      background: rgb(50,13,45);
+      background: -moz-linear-gradient(90deg, rgba(50,13,45,1) 50%, rgba(255,255,255,1) 50%);
+      background: -webkit-linear-gradient(90deg, rgba(50,13,45,1) 50%, rgba(255,255,255,1) 50%);
+      background: linear-gradient(90deg, rgba(50,13,45,1) 50%, rgba(255,255,255,1) 50%);
+      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#320d2d",endColorstr="#ffffff",GradientType=1);
+      border-bottom: 1px solid #ebebeb;*/
+      height: 50%; /*500px*/
+      margin-top: 62px;
+      padding: 0px;
+    }
+    .home-page .wrapper-logo{
+        padding: 25px;
+        background-color: #fff;
+    }
+    .home-page .jumbotron h2{
+          font-size: 85px;
+          /*color:  #320d2d;*/
+          /*background-color: #fff;*/
+          margin-bottom: -5px;
+          /*padding: 15px;*/
+          letter-spacing: 1px;
+          /*font-family: 'Milonga', cursive;*/
+          /*display:table-cell;*/
+          /*text-align:center;*/
+          /*width:100%;*/
+          /*vertical-align: middle;*/
+          /*border: 1px solid grey;*/
+    }
+    .home-page .jumbotron h3{
+          font-size: 15px;
+          text-transform: uppercase;
+          font-weight: bold;
+          text-align: center;
+          position: relative;
+          bottom: 5px;
+          left: 66px;
+          /*color:  #320d2d;*/
+    }
+
+  }
+
+  /* Small Devices, Tablets */
+  @media (min-width : 768px) {
+
+  }
+
+  /* Medium Devices, Desktops */
+  @media (min-width : 992px) {
+
+  }
+
+  /* Large Devices, Wide Screens */
+  @media (min-width : 1200px) {
+
+  }
+
+</style>
+
+
+
+
+<div class="home-page">
+
+  <section class="jumbotron center-content">
+  <div class="wrapper-logo">
+    <div class="logo">
+        <h2>Simpooly</h2>
+        <h3>Web Development</h3>
     </div>
-    <!-- <a href="#top"><i class="fa fa-angle-double-down home-arrow fa-4x" aria-hidden="true"></i></a> -->
-</section>
+  </div>
+  </section>
 
-
-<section class="section-small sec-border">
-    <div class="border-front">
-    <div class="container inline">
-        <h3>We've got what you need</h3>
-
-            <p class="float-l inline">At Simpooly our web development expertise shines with creating fully customizable websites for your buisness and personal use. Focusing on simple web designs, useablility, and user experience.</p>
-            <button type="button" class="btn btn-secondary btn-lg dark float-r inline"><a href="portfolio.php">View our work</a></button>
-            </div>
+  <section class="motto">
+    <div class="Wrapper center-content">
+      <h3>We've got what you need</h3>
     </div>
-</section>
+  </section>
 
+  <section class="services">
+    <div class="Wrapper">
+      <h3>At your Service</h3>
+      <div>
+        <ul class="service-container text-center">
 
-<section class="section-full service vertical-cut">
-    <div class="full-width">
-        <h3>At Your Service</h3>
+          <!-- Responsive -->
+          <li class="col-xs-12 col-lg-4 col-lg-offset-0 col-md-4 col-md-offset-0 col-sm-6">
+            <a href="$httpUrl('Search-Show','cgid','santa-cruz-collection')$">
+              <img src="Metallic_Blooms_Upsell.png?$staticlink$" alt=""  class = "">
+              <h4>Responsive Design</h4>
+              <p class="cx-brand-font">Bling! Le Classic Clog reçoit un relooking élégant dans cette édition métallique spéciale.</p>
+            </a>
+          </li>
 
-        <div class="container">
-<div class=" service-inner">
+          <!-- Customization -->
+          <li class="col-xs-12 col-lg-4 col-lg-offset-0 col-md-4 col-md-offset-0 col-sm-6">
+            <a href="$httpUrl('Search-Show','cgid','floral-print-shoes')$ ">
+              <img src="Metallic_Blooms_Upsell.png?$staticlink$" alt=""  class = "">
+              <h4>Customization and Usability</h4>
+              <p class="cx-brand-font">Bling! Le Classic Clog reçoit un relooking élégant dans cette édition métallique spéciale.</p> class="link-text padding-top">Magasiner les motifs floraux</div>
+            </a>
+          </li>
 
+          <!-- meticulous? -->
+          <li class="col-xs-12 col-lg-4 col-lg-offset-0 col-md-4 col-md-offset-0 col-sm-6">
+            <a href="$httpUrl('Search-Show','cgid','floral-print-shoes')$ ">
+              <img src="Metallic_Blooms_Upsell.png?$staticlink$" alt=""  class = "">
+              <h4>meticulous development</h4>
+              <p class="cx-brand-font">Bling! Le Classic Clog reçoit un relooking élégant dans cette édition métallique spéciale.</p> class="link-text padding-top">Magasiner les motifs floraux</div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 
+  <section class="email-cta">
+    <div class="Wrapper">
+      <h3>Be apart of something great!</h3>
+      <h4>Let's work together</h4>
+      <img src="Metallic_Blooms_Upsell.png?$staticlink$" alt=""  class = "">
+    </div>
+  </section>
 
-        <!-- Three columns of text below the carousel -->
-        <div class="row">
-          <div class="col-lg-4">
-            <i class="fas fa-laptop fa-9x"></i>
-            <h2>Responsive Design</h2>
-            <p>This design will allow your website to be easily viewed based on screen size, platform and orientation. This process consists of using flexible grids, layouts and images. Allowing your website to automatically switch to accommodate for multiple devices. </p>
+  <section class="carosel">
+    <div class="Wrapper">
+      <h3>Project Gallery</h3>
+    </div>
+  </section>
 
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <i class="fas fa-pencil-alt fa-9x"></i>
-            <h2>Customization & Usability</h2>
-            <p>We provide services to create a unique look to match your unique brand. A custom web design can help your customers flow easily through your content, reach that call to action to complete a form, pick up the phone and make a sale.</p>
-
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <i class="fas fa-heart fa-9x"></i>
-            <h2>Made with Love</h2>
-            <p>Here at Simpooly, we care about you and your business. Quality is of top importance and everything we make is designed with intention. We put our heart and soul into each image, line and word to create total satisfaction. </p>
-
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
 </div>
-    </div> <!-- /.container -->
-    </div>
-</section>
-
-<section class="section-small sec-border">
-    <div class="border-front">
-    <div class="container">
-    <!--     <h3>Let's Get In Touch!</h3> -->
-    <div class="work-together">
-        <div class="quote-top float-l">
-            <p class="float-l quote">Be apart of somthing great. </p>
-            <p class="float-l quote offset">Let's work together! <i class="fas fa-arrow-circle-right fa-1x"></i></p>
-        </div>
-        <div class="quote-bottom float-r center">
-                <a href="contact.php"><i class="fa fa-envelope fa-9x float-r"></i></a>
-        </div>
-                </div> <!-- Work together -->
-            </div><!-- Container -->
-        </div> <!-- border -->
-</section>
-<!-- <section class="section-small primary">
-    <div class="container">
-    <h3>Let's Get In Touch!</h3>
-    <a href="contact.php"><i class="fa fa-envelope fa-9x"></i></a>
-
-    </div>
-</div>
-</section> -->
 
 
-<section class="vertical-cut">
-    <div class="front-album text-muted full-width">
-        <div class="container front-gallery">
-            <h3>Recent Projects</h3>
-            <ul class="row">
-                <?php
-                $random = array_rand($project, 2);
-
-                    foreach($random as $id){
-                        echo get_item_detail($id, $project[$id]);
-                    }
-                ?>
-            </ul>
-            <button type="button" class="btn btn-secondary btn-lg secondary"><a href="portfolio.php">View More</a></button>
-        </div>
-        </div>
-</section>
 
 
 
 <?php include('inc/footer.php'); ?>
+
